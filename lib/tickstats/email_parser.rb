a115408d18235f7188d054e6b52ba14f2c174bc5
Ian Vaughan <github@ianvaughan.co.uk>
2013-02-01 18:16:56 +0000

    move self methods into class self

--- lib/tickstats/email_parser.rb
+++ lib/tickstats/email_parser.rb
@@ -3,30 +3,33 @@ require 'date'
 module TickStats
   class EmailParser
 
-    def self.parse text
-      return '' unless text
-      date = ''
-      names = {}
-      text.each_line do |line|
-        date = parse_date(find_date(line)) if line =~ /^The following people/
-        names.merge!(extract_name_hour(line)) if line =~ /^  /
+    class << self
+      def parse text
+        return '' unless text
+        date = ''
+        names = {}
+        text.each_line do |line|
+          date = parse_date(find_date(line)) if line =~ /^The following people/
+          names.merge!(extract_name_hour(line)) if line =~ /^  /
+        end
+        { date => names }
       end
-      { date => names }
-    end
 
-    def self.find_date text
-      test_string = 'Tickspot for '
-      index = text.index(test_string) + test_string.length
-      text.chomp[index..-2]
-    end
+      def find_date text
+        test_string = 'Tickspot for '
+        # return if text.nil? || text.index(test_string) == 0
+        index = text.index(test_string) + test_string.length
+        text.chomp[index..-2]
+      end
 
-    def self.parse_date date
-      Date.parse(date).strftime("%Y-%m-%d")
-    end
+      def parse_date date
+        Date.parse(date).strftime("%Y-%m-%d")
+      end
 
-    def self.extract_name_hour line
-      split = line.split(':')
-      { split.first.lstrip => split.last.lstrip.to_f }
+      def extract_name_hour line
+        split = line.split(':')
+        { split.first.lstrip => split.last.lstrip.to_f }
+      end
     end
 
   end
