e3200be0e46b40ee14508a374c100af259d14b0d
Ian Vaughan <github@ianvaughan.co.uk>
2013-02-01 18:31:28 +0000

    change spec input and expecting

--- spec/tickstats/email_parser_spec.rb
+++ spec/tickstats/email_parser_spec.rb
@@ -6,8 +6,8 @@ module TickStats
 
 The following people should be ashamed of themselves for not completing their time on Tickspot for Tuesday, 07 Aug '12:
 
-  First Name: 0
-  Second Name: 1.2
+  First Person: 0
+  Second Person: 1.2
 
 Please berate them at every possible opportunity."}
 
@@ -23,16 +23,16 @@ Please berate them at every possible opportunity."}
 
     it "should extract name and whole number into a hash" do
       text = "  First Name: 1"
-      EmailParser.extract_name_hour(text).should == {"First Name" => 1}
+      EmailParser.extract_name_hour(text).should == {"First Person" => 1}
     end
 
     it "should extract name and floating point number into a hash" do
       text = "  First Name: 1.2"
-      EmailParser.extract_name_hour(text).should == {"First Name" => 1.2}
+      EmailParser.extract_name_hour(text).should == {"First Person" => 1.2}
     end
 
     it "should parse emails and extract names and hours into a hash" do
-      EmailParser.parse(text).should == {"2012-08-07" => {'First Name' => 0, 'Second Name' => 1.2}}
+      EmailParser.parse(text).should == {"2012-08-07" => {'First Person' => 0, 'Second Person' => 1.2}}
     end
 
 
