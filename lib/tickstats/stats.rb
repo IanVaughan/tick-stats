6e2862ede042c5be541b9c091f7869948de7cd2c
Ian Vaughan <github@ianvaughan.co.uk>
2013-02-01 22:38:50 +0000

    sum people and daily hours

--- lib/tickstats/stats.rb
+++ lib/tickstats/stats.rb
@@ -5,6 +5,7 @@ require './lib/tickstats/email_parser'
 module TickStats
   class Stats
 
+    HOURS = 7.5
     RESULTS_FILE = 'totals.yml'
 
     def initialize logger = nil, config_file = 'config/gmail.yml'
@@ -16,6 +17,30 @@ module TickStats
     def totals
       @logger.debug "Stats::totals" if @logger
       load
+    end
+
+    def sum
+      sums = Hash.new { |hash, key| hash[key] = 0 }
+      data = load
+      data.each do |date, data|
+        data.each do |name, hours|
+          sums[name] += HOURS - hours
+        end
+      end
+      sums
+    end
+
+    def daily
+      sums = {}
+      data = load
+      data.each do |date, data|
+        daily = 0
+        data.each do |name, hours|
+          daily += HOURS - hours
+        end
+        sums[date] = daily
+      end
+      sums
     end
 
     def load reload = false
@@ -33,7 +58,7 @@ module TickStats
         result.merge! EmailParser.parse(e)
       end
 
-      File.open(@results_file, 'w') do |file|
+      File.open(RESULTS_FILE, 'w') do |file|
         file.puts result.to_yaml
       end
     end
--- spec/tickstats/stats_spec.rb
+++ spec/tickstats/stats_spec.rb
@@ -6,23 +6,27 @@ module TickStats
     let(:data) do
       {
         DateTime.new(2012, 8, 8) => {'First Person' => 0, 'Second Person' => 1.2},
-        DateTime.new(2012, 8, 8) => {'First Person' => 1, 'Second Person' => 1.8}
+        DateTime.new(2012, 8, 9) => {'First Person' => 1, 'Second Person' => 1.8}
       }
     end
 
     before do
-      Stats.any_instance.stub(:load).and_return(data)
       @stats = Stats.new
+      @stats.stub(:load).and_return(data)
     end
 
     it "should return all the current data" do
       @stats.totals.should == data
     end
 
-    # it "should get the sum all names and hours" do
-    #   @stats.stub(:load).and_return(data)
-    #   Stats.sum.should = {'First Name' => 1, 'Second Name' => 3.0}
-    # end
+    it "should get the sum all names and hours" do
+      @stats.sum.should == {'First Person' => 14.0, 'Second Person' => 12.0}
+    end
+
+    it "returns daily totals" do
+      @stats.daily.should == {DateTime.new(2012, 8, 8) => 13.8,
+                              DateTime.new(2012, 8, 9) => 12.2}
+    end
 
     # it "should load a saved file of data" do
     #   data.to_json
