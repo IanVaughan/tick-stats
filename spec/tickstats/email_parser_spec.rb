fb85c346f63a39203a110201db0ee28692b01d02
Ian Vaughan <github@ianvaughan.co.uk>
2013-02-01 23:01:32 +0000

    test with people who have not filling in their tick

--- spec/tickstats/email_parser_spec.rb
+++ spec/tickstats/email_parser_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 module TickStats
   describe EmailParser do
-    let(:text) do {
-      subject: "Tickspot Email of Shame for Tuesday, 07 Aug '12",
-      body: "Hello,
+    context "with naughty people" do
+      let(:text) do {
+        subject: "Tickspot Email of Shame for Tuesday, 07 Aug '12",
+        body: "Hello,
 
 The following people should be ashamed of themselves for not completing their time on Tickspot for Tuesday, 07 Aug '12:
 
@@ -12,28 +13,30 @@ The following people should be ashamed of themselves for not completing their ti
   Second Person: 1.2
 
 Please berate them at every possible opportunity."}
+      end
 
-    it "finds the date part" do
-      EmailParser.find_date(text[:subject]).should == "Tuesday, 07 Aug '12"
-    end
+      it "finds the date part" do
+        EmailParser.find_date(text[:subject]).should == "Tuesday, 07 Aug '12"
+      end
 
-    it "converts the date" do
-      date = "Tuesday, 07 Aug '12"
-      EmailParser.parse_date(date).should == DateTime.new(2012, 8, 7)
-    end
+      it "converts the date" do
+        date = "Tuesday, 07 Aug '12"
+        EmailParser.parse_date(date).should == DateTime.new(2012, 8, 7)
+      end
 
-    it "finds the persons name and whole number into a hash" do
-      text = "  First Person: 1"
-      EmailParser.extract_name_hour(text).should == {"First Person" => 1}
-    end
+      it "finds the persons name and whole number into a hash" do
+        text = "  First Person: 1"
+        EmailParser.extract_name_hour(text).should == {"First Person" => 1}
+      end
 
-    it "finds the persons name and floating point number into a hash" do
-      text = "  First Person: 1.2"
-      EmailParser.extract_name_hour(text).should == {"First Person" => 1.2}
-    end
+      it "finds the persons name and floating point number into a hash" do
+        text = "  First Person: 1.2"
+        EmailParser.extract_name_hour(text).should == {"First Person" => 1.2}
+      end
 
-    it "parses emails and extract names and hours into a hash" do
-      EmailParser.parse(text).should == {DateTime.new(2012, 8, 7) => {'First Person' => 0, 'Second Person' => 1.2}}
+      it "parses emails and extract names and hours into a hash" do
+        EmailParser.parse(text).should == {DateTime.new(2012, 8, 7) => {'First Person' => 0, 'Second Person' => 1.2}}
+      end
     end
 
   end
