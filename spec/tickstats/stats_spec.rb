416ccd83d77eb95df5ecdc0353b8dbd4895a4cc8
Ian Vaughan <github@ianvaughan.co.uk>
2013-02-01 20:00:36 +0000

    remove commented out code

--- spec/tickstats/stats_spec.rb
+++ spec/tickstats/stats_spec.rb
@@ -3,16 +3,14 @@ require 'spec_helper'
 module TickStats
   describe Stats do
 
-    let(:data) {
+    let(:data) do
       {
         DateTime.new(2012, 8, 8) => {'First Person' => 0, 'Second Person' => 1.2},
         DateTime.new(2012, 8, 8) => {'First Person' => 1, 'Second Person' => 1.8}
       }
-    }
+    end
 
-    # let(:stats) { Stats.new }
     before do
-      # YAML.stub(:load).and_return(data)
       Stats.any_instance.stub(:load).and_return(data)
       @stats = Stats.new
     end
@@ -22,18 +20,14 @@ module TickStats
     end
 
     # it "should get the sum all names and hours" do
-    #   stats.stub(:load).and_returns(data)
+    #   @stats.stub(:load).and_return(data)
     #   Stats.sum.should = {'First Name' => 1, 'Second Name' => 3.0}
     # end
 
     # it "should load a saved file of data" do
     #   data.to_json
-    #   stats.load
+    #   @stats.load
     # end
 
   end
 end
-
-# it "should find emails with subject 'Tickspot Email of Shame'" do
-#   pending 'moving into correct email type spec'
-# end
